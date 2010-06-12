81e4accf60c99bdd04f29ff82cf7a5edf499ec29
Dat Chu <dattanchu@gmail.com>
2010-06-12 14:32:53 -0500

    Add new flag for whether to output the visualization video or not. Remove Debug message to speed up processing of a video sequence. Encountered a bug in HighGUI on OSX: Feature grouper status window content is written to the Original Frame window instead. This problem doesn't happen on Ubuntu. Will need more tests to pin-point the issue.

--- include/feature_grouper_visualizer.h
+++ include/feature_grouper_visualizer.h
@@ -16,6 +16,7 @@ namespace SaunierSayed{
     class FeatureGrouperVisualizer {
     public:
         FeatureGrouperVisualizer(Mat homography_matrix, SaunierSayed::TrackManager *feature_grouper);
+        void ActivateDrawToFile(const cv::Size2i & frame_size, const string & output_filename = "visualizer.avi", int fourcc = CV_FOURCC('M','J','P','G'));
         void NewFrame(Mat new_frame);
         void Draw();
     private:
@@ -24,6 +25,7 @@ namespace SaunierSayed{
         Mat homography_matrix_;
         Mat image_;
         VideoWriter video_writer_;
+        bool writing_video_out_;
     };
 }
 
